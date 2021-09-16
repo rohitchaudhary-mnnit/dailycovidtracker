c6a40967dfdab08d622e54256861187c8233fc99
Rohit Chaudhary <chaudharyrohit264@gmail.com>
2021-09-17 03:28:40 +0545

    initial commit

---
+++ src/main/java/io/javaweb/dailycovidtracker/DailycovidtrackerApplication.java
@@ -0,0 +1,13 @@
+package io.javaweb.dailycovidtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailycovidtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailycovidtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/javaweb/dailycovidtracker/DailycovidtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javaweb.dailycovidtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailycovidtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
